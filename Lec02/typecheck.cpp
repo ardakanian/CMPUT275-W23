8dc425b9b71d75f60ebf0562179ef488ba228691
Omid Ardakanian <oardakan@ualberta.ca>
2023-01-11 22:30:24 -0700

    clean up

--- Lec02/typecheck.cpp
+++ Lec02/typecheck.cpp
@@ -8,8 +8,7 @@
 #include <iostream>
 using namespace std;
 
-void var_size()
-{
+void var_size() {
 	cout << "char takes " << sizeof(unsigned char) << " bytes" << endl;
 	cout << "short takes " << sizeof(short) << " bytes" << endl;
 	cout << "int takes " << sizeof(int) << " bytes" << endl;
@@ -17,26 +16,28 @@ void var_size()
 	cout << "long long takes " << sizeof(long long) << " bytes" << endl;
 	cout << "float takes " << sizeof(float) << " bytes" << endl;
 	cout << "double takes " << sizeof(double) << " bytes" << endl;
-	cout << "long double takes " << sizeof(long double) << " bytes" << endl;
+	cout << "long double takes " << sizeof(long double) << " bytes" << endl << endl;
 }
 
-int main() 
-{
+int main() {
 	var_size();
 
 	int num = 0x7c;
 	cout << hex << "hex format: " << num << endl;
 	cout << dec << "dec format: " << num << endl;
-	cout << oct << "octal format: " << num << dec << endl;
-
-	short snum = 10;
+	cout << oct << "octal format: " << num << dec << endl << endl;
 
 	cout << sizeof -1.23E-3F << endl;
+	cout << sizeof -1.23E-3 << endl << endl;
+
 	cout << sizeof "cat" << endl;
+	cout << sizeof "c" << endl;
+	cout << sizeof 'c' << endl << endl;
 
 	double dnum = -1.23E-3F;
 	cout << sizeof dnum << endl;
 
+	short snum = 10;
 	auto total = num + snum;
 	cout << sizeof total << endl;
 
