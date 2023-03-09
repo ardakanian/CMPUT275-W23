6a30df70c4aac2437a77bb72b1054eb6781d0441
Omid Ardakanian <oardakan@ualberta.ca>
2023-03-08 23:05:22 -0700

    added some comments

--- Lec15/pqueue_test.cpp
+++ Lec15/pqueue_test.cpp
@@ -2,32 +2,38 @@
 #include <utility>		// std::pair
 #include <queue>		// std::priority_queue
 #include <vector>		// std::vector
-#include <functional>	// std::greater
+#include <functional>	// std::greater & std::less
 
 using namespace std;
 
 // Create a new type name for pair<string, int> 
 typedef pair<string, int> PSI;
 
+// Objects of this class are called "function objects" 
+// because the function call operator is defined for them
 class ComparableGreater {
 public:
 	// Define the function call operator: operator()
 	bool operator() (const PSI& lhs, const PSI& rhs) const {
-		return (lhs.second > rhs.second); // min heap
+		return (lhs.second > rhs.second); // used in min heap
 	}
 };
 
+// Objects of this class are called "function objects" 
+// because the function call operator is defined for them
 class ComparableLess {
 public:
 	bool operator() (const PSI& lhs, const PSI& rhs) const {
-		return (lhs.second < rhs.second); // max heap
+		return (lhs.second < rhs.second); // used in max heap
 	}
 };
 
 int main() {
+	// The second parameter passed to the constructor 
+	// is a container for storinig elements in priority_queue.
 	// Passing std::greater<T> as the third template parameter will
 	// cause the smallest item to appear as the top()
-	// std::greater<T> is a 'function object' for performing comparisons
+	// std::greater<T> is a 'function object' for performing comparisons.
 	priority_queue<int, std::vector<int>, std::greater<int>> minheap;
 	
 	minheap.push(70);
