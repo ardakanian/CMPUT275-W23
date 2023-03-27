69fe7217a65abc8c0d3d9c1602b158b38b7d696b
Omid Ardakanian <oardakan@ualberta.ca>
2023-03-27 11:52:18 -0600

    binary search impl

---
+++ Lec20/bsearch.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>  // sort
+
+using namespace std; 
+// Be careful! There's a function called 
+// binary_search in namespace std
+
+void bsearch(vector<int>& sorted_array, int q) {
+	int high = sorted_array.size(), low = 0, mid;
+	while (low < high) {
+		mid = (high + low)/2;
+		if (sorted_array[mid] == q) {
+			// index of the integer in sorted_array
+			cout << mid << endl;
+			return;
+		} else if (sorted_array[mid] > q) {
+			high = mid;
+		} else {
+			low = mid + 1;
+		}
+	}
+	cout << "Not found" << endl;
+	return;
+}
+
+int main() {
+	int count, temp, query;
+	cin >> count;
+
+	vector<int> sorted_array;
+
+	for (int i=0;i<count;++i) {
+		cin >> temp;
+		sorted_array.push_back(temp);
+	}
+
+	// sort integers in non-descending order
+	sort(sorted_array.begin(), sorted_array.end());
+	for(int i : sorted_array)
+		cout << i << " ";
+	cout << endl;
+
+	cin >> query;
+	bsearch(sorted_array, query);
+
+	return 0;
+}
